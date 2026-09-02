91a180bbdd4efd24dc5f6cd1a2459ae59d3da43f
Gustavo Sanchez S <Gussy72@users.noreply.github.com>
2026-09-02 17:39:54 -0600

    [MIG] disable obsolete stock adhoc views

--- stock_ux/__manifest__.py
+++ stock_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Stock UX",
-    "version": "19.0.1.16.4",
+    "version": "19.0.1.16.5",
     "category": "Warehouse Management",
     "sequence": 14,
     "summary": "",
--- stock_ux/migrations/19.0.1.16.5/pre-migration.py
+++ stock_ux/migrations/19.0.1.16.5/pre-migration.py
@@ -5,7 +5,7 @@ _logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
-    """Disable child views whose models are initialized later in the graph."""
+    """Disable views left by obsolete stock compatibility modules."""
     cr.execute(
         """
         UPDATE ir_ui_view AS view
@@ -16,8 +16,8 @@ def migrate(cr, version):
                   FROM ir_model_data AS data
                  WHERE data.model = 'ir.ui.view'
                    AND data.res_id = view.id
-                   AND data.module IN ('stock_voucher', 'l10n_ar_stock_adhoc', 'l10n_ar_stock_ux')
+                   AND data.module IN ('stock_voucher', 'l10n_ar_stock_adhoc')
            )
         """
     )
-    _logger.info("Disabled %s stock child views loaded later", cr.rowcount)
+    _logger.info("Disabled %s obsolete stock compatibility views", cr.rowcount)
